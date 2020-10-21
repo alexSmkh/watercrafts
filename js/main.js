a97835b2d63bd78087a8263e8b229af54e129063
alexSmkh <alexshikari9333@gmail.com>
2020-10-21 14:28:38 +0300

    Added searching tabs

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(function () {
-
   $(".banner-section__slider").slick({
     dots: true,
     prevArrow:
@@ -8,4 +7,12 @@ $(function () {
       '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg"></button>',
   });
 
+  $(".search__tabs-item").on("click", function (e) {
+    e.preventDefault();
+    $(".search__tabs-item").removeClass("search__tabs-item--active");
+    $(".search__content-item").removeClass("search__content-item--active");
+
+    $(this).addClass("search__tabs-item--active");
+    $($(this).attr("href")).addClass("search__content-item--active");
+  });
 });
